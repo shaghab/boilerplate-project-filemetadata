404d2456bac2ea31e1e79a3ea808c0c1614689ac
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-03 15:56:56 +0500

    Update server.js
    formatted

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
